84b7674a25bce90f5b2d03ee184fcd7544490c55
Bartłomiej Bielak <bb305496@student.polsl.pl>
2025-08-18 14:46:08 +0200

    Track details done

--- frontend/src/TrackDetails.jsx
+++ frontend/src/TrackDetails.jsx
@@ -16,14 +16,17 @@ function TrackDetails({track, isOpen, onClose}) {
     return (
         <div className={"details-container"} onClick={handelBackdropClick}>
             <div className="track-details-container">
-                <div>{track.name}</div>
+                <div className="track-details-name">{track.name}</div>
                 <div><img className="track-details-image-size" src={`${PATH}${track.track_img}`}/></div>
-                <div>{track.track_length}</div>
-                <div>{track.best_time}</div>
-                <div>{track.car}</div>
-                <div>{track.country}</div>
-                <div>{track.date}</div>
-                <div>{track.driver}</div>
+                <div className="track-details-details">
+                    <div className="track-title-info">Track info</div>
+                    <div><div>Length:</div> {track.track_length}km</div>
+                    <div><div>Country:</div> {track.country}</div>
+                    <div><div>Best time:</div> {track.best_time}</div>
+                    <div><div>Car:</div> {track.car}</div>
+                    <div><div>Date:</div> {track.date}</div>
+                    <div><div>Driver:</div> {track.driver}</div>
+                </div>
             </div>
         </div>
     )
